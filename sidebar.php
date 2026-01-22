ecfe15c81b1df68a725e039ef2890579abb5e616
vanish0077 <161735440+vanish0077@users.noreply.github.com>
2026-01-22 16:54:16 +0500

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<?php
+// sidebar.php в репозитории
+?>
+<h3>Меню</h3>
+<ul>
+    <li><a href="?page=create">Создать страницу</a></li>
+    <li><a href="?page=upgrade">Обновление</a></li>
+    <li><a href="?page=parser">Парсер</a></li>
+</ul>
+<hr>
+<small>Версия системы: 1.0.2</small>
